23da74b6f1624ef685a2c04a828d838bcc0fa82c
Saikat Das <saikat@ifarmer.asia>
2024-09-13 01:09:05 +0600

    added images

--- app/src/main/java/com/example/iimagepicker/MainActivity.java
+++ app/src/main/java/com/example/iimagepicker/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.iimagepicker;
 
+import android.Manifest;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -28,39 +31,25 @@ import com.otaliastudios.cameraview.VideoResult;
 import java.util.ArrayList;
 import java.util.List;
 
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.AppSettingsDialog;
 import pub.devrel.easypermissions.EasyPermissions;
 import pub.devrel.easypermissions.PermissionRequest;
 
-public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
+public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks,
+        EasyPermissions.RationaleCallbacks {
 
 
     private BottomSheetBehavior sheetBehavior;
-//    private RelativeLayout bottom_sheet;
-//    RecyclerView peekRecyclerView , MainRecyclerView ;
-//    CameraKitView cameraKitView;
-//    RelativeLayout peekView, collapsedView;
     private ActivityMainBinding binding;
-    private String[] permission13 = new String[]{
-            android.Manifest.permission.READ_MEDIA_IMAGES,
-            android.Manifest.permission.READ_MEDIA_VIDEO,
-            android.Manifest.permission.RECORD_AUDIO
-
-    };
+    private static final int RC_CAMERA_PERM = 123;
+    private static final int RC_STORAGE_PERM = 124;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-
-        EasyPermissions.requestPermissions(
-                new PermissionRequest.Builder(this, RC_CAMERA_AND_LOCATION, {Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION})
-                        .setRationale(R.string.camera_and_location_rationale)
-                        .setPositiveButtonText(R.string.rationale_ask_ok)
-                        .setNegativeButtonText(R.string.rationale_ask_cancel)
-                        .setTheme(R.style.my_fancy_style)
-                        .build());
 
         binding.mainContent.camera.setLifecycleOwner(this);
 
@@ -78,33 +67,31 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
             // And much more
         });
 
-//        cameraKitView = findViewById(R.id.camera);
-//        bottom_sheet = findViewById(R.id.bottom_sheet);
-//        peekRecyclerView = findViewById(R.id.peekRecyclerView);
-//        peekView = findViewById(R.id.peekView);
-//        collapsedView = findViewById(R.id.collapsedView);
-//        MainRecyclerView = findViewById(R.id.MainRecyclerView);
+        binding.bottomSheet.clickImage.setOnClickListener(view -> cameraTask());
+        binding.bottomSheet.ivGallery.setOnClickListener(view -> Log.e("TAG", "onCreate: " + getAllShownImagesPath()));
+
         sheetBehavior = BottomSheetBehavior.from(binding.bottomSheet.bottomSheet);
 
-        Log.e("TAG", "onCreate: "+getAllShownImagesPath().size());
-//
         binding.bottomSheet.peekRecyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this, RecyclerView.HORIZONTAL, false));
-        binding.bottomSheet.peekRecyclerView.setAdapter(new HomeAdapter(MainActivity.this,getAllShownImagesPath()));
+        binding.bottomSheet.peekRecyclerView.setAdapter(new HomeAdapter(MainActivity.this, getAllShownImagesPath()));
 
         binding.bottomSheet.mainRecyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this, 4));
-        binding.bottomSheet.mainRecyclerView.setAdapter(new HomeAdapter(MainActivity.this,getAllShownImagesPath()));
+        binding.bottomSheet.mainRecyclerView.setAdapter(new HomeAdapter(MainActivity.this, getAllShownImagesPath()));
         binding.bottomSheet.mainRecyclerView.scrollToPosition(0);
 
+        binding.bottomSheet.ivSwitchCamera.setOnClickListener(view -> {
+            binding.mainContent.camera.toggleFacing();
+        });
 
         binding.bottomSheet.mainRecyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
-                if(!binding.bottomSheet.mainRecyclerView.canScrollVertically(-1)){
-                    if(sheetBehavior.isDraggable()== false){
+                if (!binding.bottomSheet.mainRecyclerView.canScrollVertically(-1)) {
+                    if (sheetBehavior.isDraggable() == false) {
                         sheetBehavior.setDraggable(true);
                     }
-                }else{
-                    if(sheetBehavior.isDraggable()==true){
+                } else {
+                    if (sheetBehavior.isDraggable() == true) {
                         sheetBehavior.setDraggable(false);
                     }
                 }
@@ -118,7 +105,7 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
 
             @Override
             public void onSlide(@NonNull View view, float v) {
-                binding.bottomSheet.peekView.setAlpha(1.0f-v);
+                binding.bottomSheet.peekView.setAlpha(1.0f - v);
                 binding.bottomSheet.collapsedView.setAlpha(v);
             }
         });
@@ -172,24 +159,88 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
         Cursor cursor;
         int column_index_data, column_index_folder_name;
         ArrayList<String> listOfAllImages = new ArrayList<String>();
-        String absolutePathOfImage = null;
-        uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = new String[]{
+                MediaStore.Images.Media._ID,
+                MediaStore.Images.Media.BUCKET_DISPLAY_NAME,
+                MediaStore.Images.Media.DATE_TAKEN
+        };
 
-        String[] projection = { MediaStore.MediaColumns.DATA,
-                MediaStore.Images.Media.BUCKET_DISPLAY_NAME };
+// content:// style URI for the "primary" external storage volume
+        Uri images = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
 
-        cursor = getContentResolver().query(uri, projection, null,
-                null, null);
+// Make the query.
+        Cursor cur = getContentResolver().query(images,
+                projection, // Which columns to return
+                null,       // Which rows to return (all rows)
+                null,       // Selection arguments (none)
+                null        // Ordering
+        );
 
-        column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
-        column_index_folder_name = cursor
-                .getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
-        while (cursor.moveToNext()) {
-            absolutePathOfImage = cursor.getString(column_index_data);
+        Log.e("ListingImages", " query count=" + cur.getCount());
 
-            listOfAllImages.add(absolutePathOfImage);
+        if (cur.moveToFirst()) {
+            String bucket;
+            String date;
+            int bucketColumn = cur.getColumnIndex(
+                    MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
+
+            int dateColumn = cur.getColumnIndex(
+                    MediaStore.Images.Media.DATE_TAKEN);
+
+            do {
+                // Get the field values
+                bucket = cur.getString(bucketColumn);
+                date = cur.getString(dateColumn);
+
+                // Do something with the values.
+                Log.e("ListingImages", " bucket=" + bucket
+                        + "  date_taken=" + date);
+            } while (cur.moveToNext());
         }
+//        String absolutePathOfImage = null;
+//        uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+//
+//        String[] projection = { MediaStore.MediaColumns.DATA,
+//                MediaStore.Images.Media.BUCKET_DISPLAY_NAME };
+//
+//        cursor = getContentResolver().query(uri, projection, null,
+//                null, null);
+//
+//        column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+//        column_index_folder_name = cursor
+//                .getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
+//        while (cursor.moveToNext()) {
+//            absolutePathOfImage = cursor.getString(column_index_data);
+//
+//            listOfAllImages.add(absolutePathOfImage);
+//        }
+
         return listOfAllImages;
+    }
+
+
+    private boolean hasCameraPermission() {
+        return EasyPermissions.hasPermissions(this, Manifest.permission.CAMERA);
+    }
+
+
+    private boolean hasStoragePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            return EasyPermissions.hasPermissions(
+                    this,
+                    getString(R.string.rationale_camera),
+                    Manifest.permission.READ_MEDIA_VIDEO,
+                    Manifest.permission.READ_MEDIA_IMAGES
+            );
+        } else {
+            return EasyPermissions.hasPermissions(
+                    this,
+                    getString(R.string.rationale_camera),
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            );
+        }
+        // return EasyPermissions.hasPermissions(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
     }
 
     @Override
@@ -199,6 +250,61 @@ public class MainActivity extends AppCompatActivity implements EasyPermissions.P
 
     @Override
     public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            new AppSettingsDialog.Builder(this).build().show();
+        }
+    }
 
+    @Override
+    public void onRationaleAccepted(int requestCode) {
+
+    }
+
+    @Override
+    public void onRationaleDenied(int requestCode) {
+
+    }
+
+    @AfterPermissionGranted(RC_CAMERA_PERM)
+    public void cameraTask() {
+        if (hasCameraPermission()) {
+            // Have permission, do the thing!
+            Toast.makeText(this, "TODO: Camera things", Toast.LENGTH_LONG).show();
+        } else {
+            // Ask for one permission
+            EasyPermissions.requestPermissions(
+                    this,
+                    getString(R.string.rationale_camera),
+                    RC_CAMERA_PERM,
+                    Manifest.permission.CAMERA);
+        }
+    }
+
+    @AfterPermissionGranted(RC_STORAGE_PERM)
+    public void storageTask() {
+        if (hasStoragePermission()) {
+            // Have permission, do the thing!
+            Toast.makeText(this, "TODO: Camera things", Toast.LENGTH_LONG).show();
+        } else {
+            // Ask for one permission
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                EasyPermissions.requestPermissions(
+                        this,
+                        getString(R.string.rationale_camera),
+                        RC_STORAGE_PERM,
+                        Manifest.permission.READ_MEDIA_VIDEO,
+                        Manifest.permission.READ_MEDIA_IMAGES
+                );
+            } else {
+                EasyPermissions.requestPermissions(
+                        this,
+                        getString(R.string.rationale_camera),
+                        RC_STORAGE_PERM,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                );
+            }
+        }
     }
 }
